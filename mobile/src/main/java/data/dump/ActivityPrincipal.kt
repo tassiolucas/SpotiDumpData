68b5f09855976bcaf44600aa86cf84a0ddb104df
Tássio Lucas <tassiolucas@live.com>
2018-08-03 01:34:49 -0300

    -inicializa e envia o projeto para o repositório

---
+++ mobile/src/main/java/data/dump/ActivityPrincipal.kt
@@ -0,0 +1,12 @@
+package data.dump
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
